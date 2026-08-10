5500c694c40f78cbaa6820524cc67f5ee57aa071
Maxi N. <98279618+CenturyRV@users.noreply.github.com>
2026-08-10 17:44:49 +0200

    KittyLogo: the cat on the home button now takes on your accent colour

--- src/kittycordplugins/kittyLogo/index.tsx
+++ src/kittycordplugins/kittyLogo/index.tsx
@@ -5,10 +5,15 @@
  */
 
 import { definePluginSettings } from "@api/Settings";
+import { disableStyle, enableStyle } from "@api/Styles";
+import { classNameFactory } from "@utils/css";
 import { Logger } from "@utils/Logger";
 import definePlugin, { OptionType, PluginNative } from "@utils/types";
 
 import { BRAND_ICON } from "../../branding";
+import style from "./style.css?managed";
+
+const cl = classNameFactory("vc-kittylogo-");
 
 const Native = VencordNative?.pluginHelpers?.KittyLogo as PluginNative<typeof import("./native")> | undefined;
 const logger = new Logger("KittyLogo");
@@ -37,6 +42,12 @@ const settings = definePluginSettings({
         description: "Also replace the Discord taskbar / app icon with the Kittycord cat",
         default: true,
         onChange: value => (value ? enableAppIcon() : disableAppIcon())
+    },
+    accentTint: {
+        type: OptionType.BOOLEAN,
+        description: "Tint the cat on the home button with your accent colour",
+        default: true,
+        onChange: value => (value ? enableStyle(style) : disableStyle(style))
     }
 });
 
@@ -51,6 +62,7 @@ export default definePlugin({
         return (
             <img
                 src={BRAND_ICON}
+                className={cl("icon")}
                 width={36}
                 height={36}
                 style={{ borderRadius: "30%", objectFit: "contain" }}
@@ -72,9 +84,11 @@ export default definePlugin({
 
     start() {
         if (settings.store.taskbarIcon) enableAppIcon();
+        if (settings.store.accentTint) enableStyle(style);
     },
 
     stop() {
         disableAppIcon();
+        disableStyle(style);
     }
 });
